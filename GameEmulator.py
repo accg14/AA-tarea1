ab69ea80e9ca51f05f73fa46dcdfc67964d4123f
joaguirrem <joaquin.aguirre@fing.edu.uy>
2019-03-12 00:54:58 -0300

    Fix

--- GameEmulator.py
+++ GameEmulator.py
@@ -21,31 +21,30 @@ class Game:
 	PLAYER_1_X_WIN = 12
 	PLAYER_2_X_WIN = 4
 
-	def __init__(self):
-		now = datetime.datetime.now()
-		name = "snapshots-" + str(now).replace(':','_') + ".txt"
 
+	def __init__(self):
 		self.generalizer = Generalizer()
 
-		self.name = name 
-		self.player_turn = 1
+		self.name = "result.txt" 
+		self.player_turn = self.PLAYER_ONE
+
 		self.board = self.create_board()
 		self.players_pieces = self.initialize_players_pieces()
+		self.moves_limit = 50000
 
-		self.player1_start_pieces = 10
-		self.player1_far_pieces = 0
-		self.player1_middle_pieces = 0
-		self.player1_near_pieces = 0
-		self.player1_end_pieces = 0
-
-		self.player2_start_pieces = 10
-		self.player2_far_pieces = 0
-		self.player2_middle_pieces = 0
-		self.player2_near_pieces = 0
-		self.player2_end_pieces = 0
-
+		self.player1_end_pieces = self.EMPTY
+		self.player2_end_pieces = self.EMPTY
+		self.player1_near_pieces = self.EMPTY
+		self.player2_near_pieces = self.EMPTY
+		self.player1_middle_pieces = self.EMPTY
+		self.player2_middle_pieces = self.EMPTY
+		self.player1_far_pieces = self.EMPTY
+		self.player2_far_pieces = self.EMPTY
+		self.player1_start_pieces = self.PIECES
+		self.player2_start_pieces = self.PIECES
 
 		self.move_identifier = 0
+
 
 	def initialize_players_pieces(self):
 		players_pieces = []
@@ -53,6 +52,7 @@ class Game:
 		players_pieces.append([[0,4], [1,4], [1,5], [2,3], [2,4], [2,5], [3,2], [3,3], [3,4], [3,5]])
 		players_pieces.append([[16,4], [15,4], [15,5], [14,3], [14,4], [14,5], [13,2], [13,3], [13,5], [13,5]])
 		return players_pieces
+
 
 	def create_board_line(self, start, end, value):
 		row = []
@@ -62,6 +62,7 @@ class Game:
 			else:
 				row.append(self.INVALID)
 		return row
+
 
 	def create_board(self):
 		board = []
@@ -84,6 +85,7 @@ class Game:
 		board.append(self.create_board_line(4, 4, self.PLAYER_TWO))
 		return board
 
+
 	def start_game(self):
 		while not self.GAME_OVER:
 			self.move_weighted()
@@ -94,6 +96,7 @@ class Game:
 				self.move_randomly()
 				self.move_identifier += 1
 				self.game_over()
+
 
 	def update_var(self, old_pos, new_pos):
 		if (self.player_turn == self.PLAYER_ONE):
@@ -142,6 +145,7 @@ class Game:
 			else:
 				self.player2_end_pieces += 1
 
+
 	def execute_move(self, id_piece, new_pos):
 		old_pos = self.players_pieces[self.player_turn][id_piece]
 		self.board[old_pos[0]][old_pos[1]] = self.EMPTY
@@ -153,6 +157,7 @@ class Game:
 
 		self.player_turn = self.player_turn % 2 + 1
 
+
 	def move_randomly(self):
 		id_piece = random.randint(0, self.PIECES - 1)
 		possibilities = self.calculate_moves(self.players_pieces[self.player_turn][id_piece])
@@ -161,6 +166,7 @@ class Game:
 			self.execute_move(id_piece, new_pos)
 		else:
 			self.move_randomly()
+
 
 	def move_weighted(self):
 		greatest_profit = -1.0
@@ -174,12 +180,7 @@ class Game:
 			print("PIECES: ", str(i), "possibilities: ", str(possibilities))
 			for possible_move in possibilities:
 				current_profit = self.simulate_state(i, possible_move)
-				if (current_profit > 0.50):
-					piece = i
-					move = possible_move
-					stop = True
-					break
-				elif (current_profit > greatest_profit):
+				if (current_profit > greatest_profit):
 					greatest_profit = current_profit
 					piece = i
 					move = possible_move
@@ -188,50 +189,52 @@ class Game:
 		print("current_profit: ", str(current_profit), " piece: ", str(piece), " move: ", str(move))
 		self.execute_move(piece, move)
 
+
 	def simulate_state(self, id_piece, move_to_test):
 		old_pos = self.players_pieces[self.player_turn][id_piece]
+
+		# Simulates 'fake' movement
 		self.update_var(old_pos, move_to_test)
 
 		player_one_weight = self.player1_end_pieces*self.generalizer.get_player1_end_weight() + self.player1_near_pieces*self.generalizer.get_player1_near_weight() + self.player1_middle_pieces*self.generalizer.get_player1_middle_weight() + self.player1_far_pieces*self.generalizer.get_player1_far_weight() + self.player1_start_pieces*self.generalizer.get_player1_start_weight()
-
 		player_two_weight = self.player2_end_pieces*self.generalizer.get_player2_end_weight() + self.player2_near_pieces*self.generalizer.get_player2_near_weight() + self.player2_middle_pieces*self.generalizer.get_player2_middle_weight() + self.player2_far_pieces*self.generalizer.get_player2_far_weight() + self.player2_start_pieces*self.generalizer.get_player2_start_weight()
 
-		self.update_var(move_to_test, old_pos) # undo 'fake' movement
+		# Undo 'fake' movement
+		self.update_var(move_to_test, old_pos)
 
 		return (player_one_weight + player_two_weight + self.generalizer.get_independent_weight())
 
+
 	def game_over(self):
-		if(self.player1_end_pieces == self.PIECES or self.player2_end_pieces == self.PIECES):
+		if(self.player1_end_pieces == self.PIECES or self.player2_end_pieces == self.PIECES or self.moves_limit < self.move_identifier):
 			self.GAME_OVER = True
-			self.generalizer.adjust_weights()
+			if (self.player1_end_pieces == self.PIECES):
+				self.generalizer.adjust_weights(1)
+			else:
+				self.generalizer.adjust_weights(-1)
 
 		self.save_state()
 
+
 	def save_state(self):
+		split = "-"
+
 		file = open(self.name, "a")
-		
-		line = str(self.generalizer.get_independent_weight()) + "-"
-		line += str(self.generalizer.get_player1_start_weight()) + "-"
-		line += str(self.generalizer.get_player1_far_weight()) + "-"
-		line += str(self.generalizer.get_player1_middle_weight()) + "-"
-		line += str(self.generalizer.get_player1_near_weight()) + "-"
-		line += str(self.generalizer.get_player1_end_weight()) + "-"
-		line += str(self.generalizer.get_player2_start_weight()) + "-"
-		line += str(self.generalizer.get_player2_far_weight()) + "-"
-		line += str(self.generalizer.get_player2_middle_weight()) + "-"
-		line += str(self.generalizer.get_player2_near_weight()) + "-"
-		line += str(self.generalizer.get_player2_end_weight()) + "-"
+
+		line = str(self.player1_end_pieces) + split
+		line += str(self.player2_end_pieces) + split
+		line += str(self.player1_near_pieces) + split
+		line += str(self.player2_near_pieces) + split
+		line += str(self.player1_middle_pieces) + split
+		line += str(self.player2_middle_pieces) + split
+		line += str(self.player1_far_pieces) + split
+		line += str(self.player2_far_pieces) + split
+		line += str(self.player1_start_pieces) + split
+		line += str(self.player2_start_pieces) + "\n"
 
 		file.write(line)
-
-		if self.GAME_OVER:
-			if (self.player1_end_pieces == self.PIECES):
-				winner = 1
-			else:
-				winner = -1
-			file.write(str(winner) + "\n")
-
 		file.close()
+
 
 	def calculate_moves(self, position):
 		possibilities = []
@@ -331,8 +334,10 @@ class Game:
 
 		return possibilities
 
+
 	def verify_jump(self, position):
 		return position[0] in range(self.X_MIN,self.X_MAX) and position[1] in range(self.Y_MIN,self.Y_MAX) and self.board[position[0]][position[1]] == self.EMPTY
+
 
 if __name__== "__main__":
 	Test = Game()
--- Generalizer.py
+++ Generalizer.py
@@ -1,96 +1,84 @@
 class Generalizer:
 	def __init__(self):
-		#self.independent_weight = 0.001
-
-		#self.player1_start_weight = 0.01
-		#self.player1_far_weight = 0.025
-		#self.player1_middle_weight = 0.05
-		#self.player1_near_weight = 0.075
-		#self.player1_end_weight = 0.1
-
-		#self.player2_start_weight = 0.02
-		#self.player2_far_weight = 0.01
-		#self.player2_middle_weight = -0.04
-		#self.player2_near_weight = -0.06
-		#self.player2_end_weight = -0.1
-
-		self.weights = [0.001, 0.01, 0.025, 0.05, 0.075, 0.1, 0.02, 0.01, -0.04, -0.06, -0.1]
-
+		self.weights = [0.001, 0.1, -0.1, 0.075, -0.075, 0.05, -0.025, -0.025, 0.025, -0.05, 0.05]
 		self.mu = 0.05
-
 		self.print()
+
 
 	def get_independent_weight(self):
 		return self.weights[0]
 
-	def get_player1_start_weight(self):
+	def get_player1_end_weight(self):
 		return self.weights[1]
 
-	def get_player1_far_weight(self):
+	def get_player2_end_weight(self):
 		return self.weights[2]
 
-	def get_player1_middle_weight(self):
+	def get_player1_near_weight(self):
 		return self.weights[3]
 
-	def get_player1_near_weight(self):
+	def get_player2_near_weight(self):
 		return self.weights[4]
 
-	def get_player1_end_weight(self):
+	def get_player1_middle_weight(self):
 		return self.weights[5]
 
-	def get_player2_start_weight(self):
+	def get_player2_middle_weight(self):
 		return self.weights[6]
 
-	def get_player2_far_weight(self):
+	def get_player1_far_weight(self):
 		return self.weights[7]
 
-	def get_player2_middle_weight(self):
+	def get_player2_far_weight(self):
 		return self.weights[8]
 
-	def get_player2_near_weight(self):
+	def get_player1_start_weight(self):
 		return self.weights[9]
 
-	def get_player2_end_weight(self):
+	def get_player2_start_weight(self):
 		return self.weights[10]
+
 
 	def set_independent_weight(self, value):
 		self.weights[0] = value
 
-	def set_player1_start_weight(self, value):
+	def set_player1_end_weight(self, value):
 		self.weights[1] = value
 
-	def set_player1_far_weight(self, value):
+	def set_player2_end_weight(self, value):
 		self.weights[2] = value
 
-	def set_player1_middle_weight(self, value):
+	def set_player1_near_weight(self, value):
 		self.weights[3] = value
 
-	def set_player1_near_weight(self, value):
+	def set_player2_near_weight(self, value):
 		self.weights[4] = value
 
-	def set_player1_end_weight(self, value):
+	def set_player1_middle_weight(self, value):
 		self.weights[5] = value
 
-	def set_player2_start_weight(self, value):
+	def set_player2_middle_weight(self, value):
 		self.weights[6] = value
 
-	def set_player2_far_weight(self, value):
+	def set_player1_far_weight(self, value):
 		self.weights[7] = value
 
-	def set_player2_middle_weight(self, value):
+	def set_player2_far_weight(self, value):
 		self.weights[8] = value
 
-	def set_player2_near_weight(self, value):
+	def set_player1_start_weight(self, value):
 		self.weights[9] = value
 
-	def set_player2_end_weight(self, value):
+	def set_player2_start_weight(self, value):
 		self.weights[10] = value
+
 
 	def parse_number(s):
 		return int(s)
 
-	def adjust_weights(self):
-		file = open('test.txt', 'r')
+
+	def adjust_weights(self, result):
+		file = open('result.txt', 'r')
 		tuplas_resultado = []
 		tuplas_resultado.append([])
 		for line in file:
@@ -98,7 +86,7 @@ class Generalizer:
 			values[len(values)-1].replace('\n','')
 			if (len(values) > 1):
 				#tuplas_resultado.append(list(map(self.parse_number(),values)))
-				b = list(map(self.parse_number(),values)) # [1, 2, 3, 4, 5, ..., 11,]
+				b = list(map(self.parse_number(), values))
 				for j in range(0, len(b)):
 					self.weights[j] = self.weights[j] + mu*( - 1)*tuplas_resultado[i][j]	
 			else:
@@ -108,21 +96,21 @@ class Generalizer:
 			for j in range(0, len(tuplas_resultado[i])):
 				self.weights[j] = self.weights[j] + mu*(tuplas_resultado[0][0] - 1)*tuplas_resultado[i][j]
  
-
 		self.print()
 
+
 	def print(self):
-		print("Independent weight:\t", self.weights[0])
+		print("Independent weight:\t", self.get_independent_weight())
 		print()
-		print("P1 | Start weight:\t", self.weights[1])
-		print("P1 | Far weight:\t", self.weights[2])
-		print("P1 | Middle weight:\t", self.weights[3])
-		print("P1 | Near weight:\t", self.weights[4])
-		print("P1 | End weight:\t", self.weights[5])
+		print("P1 | Start weight:\t", self.get_player1_start_weight())
+		print("P1 | Far weight:\t", self.get_player1_far_weight())
+		print("P1 | Middle weight:\t", self.get_player1_middle_weight())
+		print("P1 | Near weight:\t", self.get_player1_near_weight())
+		print("P1 | End weight:\t", self.get_player1_end_weight())
 		print()
-		print("P2 | Start weight:\t", self.weights[6])
-		print("P2 | Far weight:\t", self.weights[7])
-		print("P2 | Middle weight:\t", self.weights[8])
-		print("P2 | Near weight:\t", self.weights[9])
-		print("P2 | End weight:\t", self.weights[10])
+		print("P2 | Start weight:\t", self.get_player2_start_weight())
+		print("P2 | Far weight:\t", self.get_player2_far_weight())
+		print("P2 | Middle weight:\t", self.get_player2_middle_weight())
+		print("P2 | Near weight:\t", self.get_player2_near_weight())
+		print("P2 | End weight:\t", self.get_player2_end_weight())
 		print()
